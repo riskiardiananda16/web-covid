63fd8343abc68c983da7b87a3ab9c4ca5f0c69cf
riskiardiananda16 <riskiardiananda16@gmail.com>
2020-09-30 15:31:44 +0700

    covid

---
+++ resources/views/login.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>welcome</title>
+<style>
+body{
+	background-image:url(file:///D:/[www.gigapurbalingga.net]_KMSpco1020F/_/POTO/Saved%20Pictures/bijak-inggris.jpg);
+	background-repeat: no-repeat;
+	background-size: 100% 100%;
+	background-attachment: fixed;
+}
+</style>
+</head>
+<body text="red">
+	<br>
+	<center>
+		<h1>silahkan masuk</h1>
+	<form>
+		<tr>
+				<td><b>username :</b></td>
+				<td><input type="text" name="varnama" size="30" placeholder="masukan username">
+				</td>
+			</tr>
+			<p>
+			<tr>
+				<td><b>password :</b></td>
+				<td><input type="text" name="varnama" size="30" placeholder="masukan password">
+				</td>
+			</tr>
+			<br>
+			<P>
+			<a href="riski.php" onclick="alert('apakah anda yakin')">masuk</a>
+
+</body>
+</html>
